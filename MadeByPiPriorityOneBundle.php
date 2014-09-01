d1d6ddee7d2fa75da18b6a01935f6a0dd696afb2
unknown <marks@wkstn-markspils.Adprinciples.co.uk>
2014-09-01 15:10:24 +0100

    Add initial files

---
+++ MadeByPiPriorityOneBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace WillmottDixon\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * MadeByPiPriorityOneBundle
+ */
+class MadeByPiPriorityOneBundle extends Bundle
+{
+}
